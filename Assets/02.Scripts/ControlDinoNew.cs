6b9b26d68eae9343ddaead4cbfe38d16bdd4f2e1
movinggun0219 <daugun@naver.com>
2026-03-08 13:39:48 +0900

    gap

--- Assets/02.Scripts/ControlDinoNew.cs
+++ Assets/02.Scripts/ControlDinoNew.cs
@@ -6,7 +6,7 @@ public class ControlDinoNew : MonoBehaviour
 {
     float moveSpeed = 0.1f;
     float sideSpeed = 0.1f;
-    float gap = 0.5f;
+    float gapX = 2f;
     //public Vector3 offset;
     // Start is called before the first frame update
     void Start()
@@ -20,12 +20,18 @@ public class ControlDinoNew : MonoBehaviour
         transform.Translate(new Vector3(0, 0, moveSpeed));
         if (Input.GetKey(KeyCode.A)) { transform.Translate(new Vector3(-1 * sideSpeed, 0, 0)); }
         if (Input.GetKey(KeyCode.D)) { transform.Translate(new Vector3(sideSpeed, 0, 0)); }
-        float detect = transform.childCount % 2 - 1;
-        float prev = detect / 2;
-        for (int i = 1; i <= transform.childCount; i++)
+
+        //float detect = transform.childCount % 2 - 1;
+        //float prev = detect / 2;
+        //for (int i = 1; i <= transform.childCount; i++)
+        //{
+        //    transform.GetChild(i-1).position = new Vector3(prev, 0, 0);
+        //    prev += ((i % 2) * 2 - 1) * i;
+        //}
+
+        for (int i = 0; i < transform.childCount; i++)
         {
-            transform.GetChild(i-1).position = new Vector3(prev, 0, 0);
-            prev += ((i % 2) * 2 - 1) * i;
+            transform.GetChild(i).position = new Vector3(gapX * (i + 0.5f - (float)transform.childCount / 2), 0, 0);
         }
     }
 }
